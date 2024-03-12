8ed0cf50f937db92640cb3dd5467331d5cf75b84
clarisse.lebaut <clarisse.le-baut@laplateforme.io>
2024-03-12 16:36:26 +0100

    Jour 2 - Python Work

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from tkinter import *
 import tkinter
+import random
 
-#---------------------------------------------------------------PARAMETRE DE LA FENETRE, NE PAS TOUCHER
+#---------------------------------------------------------------PARAMETRES DE LA FENETRE
 #permet d'implanter la variable dans le projet tKinter
 motdepasse= tkinter.Tk()
 #dispostion en grille de la fenêtre
 motdepasse.grid_columnconfigure((0,1), minsize=15, weight=5)
-motdepasse.grid_rowconfigure((0,1,2,3,4,5,6,7),minsize=15, weight=5)
+motdepasse.grid_rowconfigure((0,1,2,3,4,5,6,7,8),minsize=15, weight=5)
 #fond de le fenetre
 motdepasse["bg"]= '#8B8878'
 #titre de la fenêtre
@@ -22,7 +23,7 @@ titre.grid(row=0, columnspan=2)
 text_util = tkinter.Label(motdepasse, text="Nom d'utilisateur", font=("Arial", 12),background='#8B8878')
 text_util.grid(row=1, columnspan=2)
 nomutilisateur = tkinter.Entry(motdepasse)
-nomutilisateur.grid(row=2, columnspan=2, ipadx=75, ipady=5)
+nomutilisateur.grid(row=2, columnspan=2, ipadx=77, ipady=5)
 
 #barre de saisie du mot de passe
 text_mdp = tkinter.Label(motdepasse, text="Mot de passe", font=("Arial", 12), background='#8B8878')
@@ -39,6 +40,39 @@ def visibility():
         entry_mdp.config(show="")
         toggle_btn.config(image=_close)
 
+"""def connect():
+    rec = entry_mdp.get()
+    print(rec)
+"""
+def connect():
+	id = nomutilisateur.get()
+	tmdp = entry_mdp.get()
+	if id == "Clarisse" and tmdp == "1234":
+		valide.config(text="Connexion...")
+	else : 
+		msg_erreur.config(text="Erreur")
+
+valide = tkinter.Label(motdepasse, fg="green", background='#8B8878')
+valide.grid(row=5, column=1, sticky="nw")
+
+msg_erreur = tkinter.Label(motdepasse, fg="red", background='#8B8878')
+msg_erreur.grid(row=5, column=1, sticky="nw")
+
+def générer():
+    majusucle="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    minuscule="abcdefghijklmnopqrstuvwxyz"
+    spacial="%+-*/§!/:?#@"
+    chiffres = "0123456789"
+    caracteres= majusucle + minuscule + spacial + chiffres
+    n = [random.choice(caracteres) for i in range (8)]
+    print(n)
+
+def save():
+    print("On te l'enregistre")
+
+def oublie():
+    print("Un oublie ?")
+
 
 #------LES AUTRES FONCTIONS FAITES PAR ENZO ET JEREMY
 #----------------------------------------------------        
@@ -48,28 +82,28 @@ def visibility():
 text_mdp.grid(row=3, columnspan=2)
 #-frame 1
 entry_mdp_frame=tkinter.Frame(motdepasse)
-entry_mdp.grid(row=4, columnspan=2, ipadx=75, ipady=5)
+entry_mdp.grid(row=4, columnspan=2, ipadx=77, ipady=5)
 #-frame 2 dans frame 1
-toggle_btn=tkinter.Button(motdepasse, command=visibility, activebackground="#CDCDC1")
-toggle_btn_frame=tkinter.Frame(entry_mdp_frame)
-toggle_btn.grid(row=4, column=1)
+toggle_btn=tkinter.Button(motdepasse, image=_open, command=visibility, activebackground="#CDCDC1")
+toggle_btn_frame=tkinter.Frame( entry_mdp_frame)
+toggle_btn.grid(row=4, column=1, ipadx=17)
 
 #Widgets Button sup
 #ajouter command = 
-connexion_bouton = tkinter.Button(text = "Connexion", width=30, height=2, font=("Arial", 12), bg="#F5F5DC", activebackground="#CDCDC1")
-connexion_bouton.grid(row=5, columnspan=2)
+connexion_bouton = tkinter.Button(command= connect, text = "Connexion", width=30, height=2, font=("Arial", 12), bg="#F5F5DC", activebackground="#CDCDC1")
+connexion_bouton.grid(row=6, columnspan=2)
 #ajouter command = 
-generate_bouton = tkinter.Button(text = "Générer un mot de passe", width=30, height=2, font=("Arial", 12), bg="#FFF8DC", activebackground="#CDCDC1")
-generate_bouton.grid(row=6, columnspan=2)
+generate_bouton = tkinter.Button(command= générer, text = "Générer un mot de passe", width=30, height=2, font=("Arial", 12), bg="#FFF8DC", activebackground="#CDCDC1")
+generate_bouton.grid(row=7, columnspan=2)
 #ajouter command = 
-save_bouton = tkinter.Button(text = "Enregistrer", width=30, height=2, font=("Arial", 12), bg="#EEE8CD", activebackground="#CDCDC1")
-save_bouton.grid(row=7, columnspan=2)
+save_bouton = tkinter.Button(command= save, text = "Enregistrer", width=30, height=2, font=("Arial", 12), bg="#EEE8CD", activebackground="#CDCDC1")
+save_bouton.grid(row=8, columnspan=2)
 #ajouter command = 
-forgot_bouton = tkinter.Button(text = "Mot de passe oublié ?", width=30, height=2, font=("Arial", 12), bg="#CDC8B1", activebackground="#CDCDC1")
-forgot_bouton.grid(row=8, columnspan=2,pady=6)
+forgot_bouton = tkinter.Button(command= oublie, text = "Mot de passe oublié ?", width=30, height=2, font=("Arial", 12), bg="#CDC8B1", activebackground="#CDCDC1")
+forgot_bouton.grid(row=9, columnspan=2, pady=6)
 
 
-#---------------------------------------------------------------PARAMETRE DE LA FENETRE, NE PAS TOUCHER
+#---------------------------------------------------------------PARAMETRE DE LA FENETRE
 #pour recuperer la hauteur et la largeur de mon ecran
 screen_x = int(motdepasse.winfo_screenwidth())
 screen_y = int(motdepasse.winfo_screenheight())
@@ -77,7 +111,7 @@ screen_y = int(motdepasse.winfo_screenheight())
 window_x = 480
 window_y = 480
 motdepasse.geometry("480x480")
-motdepasse.resizable(width=False, height=False) 
+motdepasse.resizable(width=False, height=False) #--> cette ligne sera a supprimer pour pouvoir redilensionner la fenêtre
 #impossible de redimenssioner quand les deux en false
 #formule permettatn de récurper les dimension en retirant les bordure de la fenetre qui sera crée
 position_x = (screen_x // 2) - (window_x // 2)
